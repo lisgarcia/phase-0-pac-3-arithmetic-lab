6a88af54c83aebf3c6a8dca1c6b1acc7d80f6919
Elisabeth Garcia <lisgarciae@gmail.com>
2023-03-17 13:40:03 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+return a + b
+}
+function subtract(a, b) {
+    return a - b
+}
+function multiply(a, b) {
+    return a * b
+}
+function divide(a,b) {
+    return a / b
+}
+
+let n = 10 
+function increment(n) {
+    return (++n);
+}
+function decrement(n) {
+    return (--n);
+}
+
+function makeInt(n) {
+    parseInt(n)
+    return parseFloat(n)
+}
+
+function preserveDecimal(n) {
+    parseFloat(n, 10)
+    return parseFloat(n)
+}
